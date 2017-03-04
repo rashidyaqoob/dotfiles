0189292c444d529331e17304240ce233d07152c6
Bassam Ismail <skippednote@gmail.com>
2017-03-04 14:04:10 +0530

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,15 @@ ln -sfn $PWD/config.fish $HOME/.config/fish/
 ln -sfn $PWD/aliases $HOME/.config/fish/
 ln -sfn $PWD/paths $HOME/.config/fish/
 ln -sfn $PWD/funcs $HOME/.config/fish/
-ln -sfn $PWD/omf $HOME/.config
 
 ln -sfn $PWD/gitconfig $HOME/.gitconfig
 ln -sfn $PWD/gitignore_global $HOME/.gitignore_global
 ln -sfn $PWD/gitmessage $HOME/.gitmessage
+
+omf install nvm
+omf install pure
+omf install blt
+
+nvm install stable
+nvm alias default stable
+npm install -g yarn
