2f4aa4436db3655d2fdf37f3ea64045404e27519
Bassam Ismail <skippednote@gmail.com>
2017-05-05 17:07:10 +0530

    Update dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,19 @@ ln -sfn $PWD/funcs $HOME/.config/fish/
 ln -sfn $PWD/gitconfig $HOME/.gitconfig
 ln -sfn $PWD/gitignore_global $HOME/.gitignore_global
 ln -sfn $PWD/gitmessage $HOME/.gitmessage
+ln -sfn $PWD/vimrc $HOME/.vimrc
+
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  echo "Installing vim-plug."
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 
 omf install nvm
 omf install pure
 omf install blt
+omf install fzf
 
 nvm install stable
 nvm alias default stable
