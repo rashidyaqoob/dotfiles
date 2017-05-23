dcc7c7bd45010cfcc4ddba513050ffc4a20d1b5f
Bassam Ismail <skippednote@gmail.com>
2017-05-23 11:52:50 +0530

    Add zsh autocompletion.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 if [ ! -d $HOME/.oh-my-zsh ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 fi
 
 ln -sfn $PWD/zshrc $HOME/.zshrc
--- zshrc
+++ zshrc
@@ -3,10 +3,10 @@ ZSH_THEME="avit"
 HYPHEN_INSENSITIVE="true"
 ENABLE_CORRECTION="true"
 COMPLETION_WAITING_DOTS="true"
-plugins=(git zsh-syntax-highlighting)
+plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
 source $ZSH/oh-my-zsh.sh
-
 export LANG=en_US.UTF-8
+
 source $HOME/Code/personal/dotfiles/aliases
 source $HOME/Code/personal/dotfiles/funcs
 source $HOME/Code/personal/dotfiles/paths
